7e163b2f73b87920eee7de50fc6e2c9d039a3703
Spaceginner <ivan.demian2009@gmail.com>
2023-01-08 21:42:19 +0600

    Added `--test` flag

--- main.py
+++ main.py
@@ -8,25 +8,30 @@ def create_parser() -> argparse.ArgumentParser:
     )
 
     parser.add_argument('mode', choices=['c', 'd'], help='c - compress; d - decompress')
-    parser.add_argument('input', help='path to a file to be processed')
+    parser.add_argument('-i', '--input', help='path to a file to be processed', required=True)
 
-    parser.add_argument('-o', '--output', help='path to a file to be stored')
+    parser.add_argument('-o', '--output', help='path where to store compressed file')
+
+    parser.add_argument('--test', action='store_true')
 
     return parser
 
 
-def main(mode: str, input_filename: str, output_filename: str | None):
+def main(mode: str, input_filename: str, output_filename: str | None, test: bool):
     if mode not in ['c', 'd']:
         raise ValueError(f'argument `mode` must be \'c\' or \'d\' not {mode}')
     if mode == 'd':
         raise NotImplementedError
 
-    if output_filename is None:
-        output_filename = input_filename + ".zdr"
+    if test:
+        pass
+    else:
+        if output_filename is None:
+            output_filename = input_filename + ".zdr"
 
-    pass
+
 
 
 if __name__ == '__main__':
     args = create_parser().parse_args()
-    main(args.mode, args.input, args.output)
+    main(args.mode, args.input, args.output, args.test)
