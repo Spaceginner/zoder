31bf7d4c5c00030dbcab567703541e95b4a646c2
Spaceginner <ivan.demian2009@gmail.com>
2023-01-09 12:03:42 +0500

    Reworked encoder

--- modules/coder/encoder.py
+++ modules/coder/encoder.py
@@ -5,12 +5,11 @@ def encode(image: Image.Image) -> list[list[bool]]:
     seq_image = list(image.convert('RGB').getdata(0))
     ims_image = [seq_image[i] != 255 for i in range(len(seq_image))]
 
-    ucnzdr_image = [[bool() for _ in range(image.size[0])] for _ in range(image.size[1])]
-    i = 0
-    for x in range(image.size[1]):
-        for y in range(image.size[0]):
-            ucnzdr_image[x][y] = ims_image[i]
-            i += 1
+    ucnzdr_image = [[bool()] * image.size[0] for _ in range(image.size[1])]
+
+    x_res = image.size[0]
+    for i in range(len(ims_image)):
+        ucnzdr_image[i // x_res][i % x_res] = ims_image[i]
 
     # for x in range(image.size[1]):
     #     for y in range(image.size[0]):
